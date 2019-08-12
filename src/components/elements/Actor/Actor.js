1e51c505377b8922a3976978afc7c010cc926981
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-12 17:50:02 -0400

    Finished setup for Actor component

--- src/components/elements/Actor/Actor.js
+++ src/components/elements/Actor/Actor.js
@@ -3,9 +3,19 @@ import { IMAGE_BASE_URL } from "../../../config";
 import "./Actor.css";
 
 export default function Actor(props) {
+  const POSTER_SIZE = "w154";
   return (
-    <div>
-      <h1>Actor</h1>
+    <div className="rmdb-actor">
+      <img
+        src={
+          props.actor.profile_path
+            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.actor.profile_path}`
+            : "./images/no_image.jpg}"
+        }
+        alt="Actor Thumbnail"
+      />
+      <span className="rmdb-actor-name">{props.actor.name}</span>
+      <span className="rmdb-actor-character">{props.actor.character}</span>
     </div>
   );
 }
