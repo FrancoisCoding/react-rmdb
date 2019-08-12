62b6ab4fc27a30211db74925bd45a7b8d7b07d8a
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-12 14:34:01 -0400

    Fetched data from api and set it to state

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE
+} from "../../config";
 import HeroImage from "../elements/HeroImage/HeroImage";
 import SearchBar from "../elements/SearchBar/SearchBar";
 import FourColGrid from "../elements/FourColGrid/FourColGrid";
@@ -8,6 +15,35 @@ import Spinner from "../elements/Spinner/Spinner";
 import "./Home.css";
 
 export default class Home extends Component {
+  state = {
+    movies: [],
+    heroImage: null,
+    loading: false,
+    currentPage: 0,
+    totalPages: 0,
+    searchTerm: ""
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
+  }
+
+  fetchItems = endpoint => {
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          movies: [...this.state.movies, ...res.results],
+          heroImage: this.state.heroImage || res.results[0],
+          loading: false,
+          currentPage: res.page,
+          total: res.total_pages
+        });
+      });
+  };
+
   render() {
     return (
       <div className="rmdb-home">
